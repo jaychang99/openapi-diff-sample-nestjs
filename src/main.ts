6582cd67be41546f8225c3dcacc51876ce5be50b
jaychang99 <jaychang99@gmail.com>
2024-07-14 20:38:22 +0900

    init: setup swagger and auto-generate openapi.json when server restarts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+
+const PORT = process.env.PORT ?? 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenAPI Diff Example')
+    .setDescription('Examples APIs to demonstrate OpenAPI diff')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // write document to root path with name openapi.json
+  fs.writeFileSync('openapi.json', JSON.stringify(document, null, 2));
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
